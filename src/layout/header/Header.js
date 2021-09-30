df8556b10dbb91e09ee7269cd32e8fda1e6b01ce
ChanMyaeOo <chanmyaeoo1221gg@gmail.com>
2021-09-30 23:16:09 +0630

    added header and adjusted for responsive

--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -1,26 +1,225 @@
-import React from 'react'
-import PortalHeaderImg from '../../images/web-header.png'
-import useStyles from './styles'
+import React from "react";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import { Link } from "react-router-dom";
+import PortalHeaderImg from "../../images/web-header.png";
+import PortalLogoImg from "../../images/portal-logo.png";
+import useStyles from "./styles";
 
 const Header = () => {
     const classes = useStyles();
+
+    const handleOpenMenu = () => {
+        const resOverlay = document.getElementById("res-overlay");
+        resOverlay.style.display = 'block'
+    }
+
+    const handleCloseMenu = () => {
+        const resOverlay = document.getElementById("res-overlay");
+        resOverlay.style.display = 'none'
+    }
+
+
     return (
-        <div className={classes.header}>
-            <div className={classes.headerWrap}>
-                <div>
-                    log in
+        <div>
+            {/* Desktop View */}
+            <div className={classes.desktopViewHeader}>
+                <div className={classes.topHeader}>
+                    <div className={classes.topHeaderWrap}>
+                        <div>log in</div>
+
+                        <div>|</div>
+
+                        <div>join the membership</div>
+
+                        <img src={PortalHeaderImg} alt="portal header" />
+                    </div>
                 </div>
 
-                <div>|</div>
-
-                <div>
-                    join the membership
+                <div className={classes.headerLogoWrap}>
+                    <img
+                        src={PortalLogoImg}
+                        alt="portal logo"
+                        className={classes.headerLogo}
+                    />
                 </div>
 
-                <img src={PortalHeaderImg} alt="portal header" />
+                <div className={classes.bottomHeader}>
+                    <ul className={classes.bottomHeaderContent}>
+                        <li>
+                            <p>british story</p>
+                            <ul className={classes.innerHeaderContent}>
+                                <li>Test 1</li>
+                                <li>Test 2</li>
+                                <li>Test 3</li>
+                                <li>Test 4</li>
+                                <li>Test 5</li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <p>ask and answer</p>
+                            <ul className={classes.innerHeaderContent}>
+                                <li>Test 1</li>
+                                <li>Test 2</li>
+                                <li>Test 3</li>
+                                <li>Test 4</li>
+                                <li>Test 5</li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <p>buy and sell</p>
+                            <ul className={classes.innerHeaderContent}>
+                                <li>Test 1</li>
+                                <li>Test 2</li>
+                                <li>Test 3</li>
+                                <li>Test 4</li>
+                                <li>Test 5</li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <p>job search</p>
+                            <ul className={classes.innerHeaderContent}>
+                                <li>Test 1</li>
+                                <li>Test 2</li>
+                                <li>Test 3</li>
+                                <li>Test 4</li>
+                                <li>Test 5</li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <p>lodgment</p>
+                            <ul className={classes.innerHeaderContent}>
+                                <li>Test 1</li>
+                                <li>Test 2</li>
+                                <li>Test 3</li>
+                                <li>Test 4</li>
+                                <li>Test 5</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            {/* Android View */}
+            <div className={classes.resHeader}>
+                <div>
+                    <MenuIcon onClick={handleOpenMenu} />
+                </div>
+
+                <Link to="/">
+                    <img
+                        src={PortalLogoImg}
+                        alt="header logo"
+                        className={classes.resHeaderLogo}
+                    />
+                </Link>
+                <div>join</div>
+            </div>
+
+            <div className={classes.overlay} id="res-overlay">
+                <div className={classes.resHeaderWrap}>
+                    <img
+                        src={PortalLogoImg}
+                        alt="header logo"
+                        className={classes.resHeaderLogo}
+                    />
+                    <CloseIcon className={classes.resClose} onClick={handleCloseMenu} />
+                </div>
+
+                <ul className={classes.resHeaderContent}>
+                    <li>
+                        <div>
+                            british story{" "}
+                            <ArrowForwardIosIcon
+                                className={classes.resHeaderArrow}
+                            />
+                        </div>
+
+                        <ul className={classes.resInnerContent}>
+                            <li>test</li>
+                            <li>test</li>
+                            <li>test</li>
+                            <li>test</li>
+                            <li>test</li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <div>
+                            ask and answer{" "}
+                            <ArrowForwardIosIcon
+                                className={classes.resHeaderArrow}
+                            />
+                        </div>
+
+                        <ul className={classes.resInnerContent}>
+                            <li>test</li>
+                            <li>test</li>
+                            <li>test</li>
+                            <li>test</li>
+                            <li>test</li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <div>
+                            buy and sell{" "}
+                            <ArrowForwardIosIcon
+                                className={classes.resHeaderArrow}
+                            />
+                        </div>
+
+                        <ul className={classes.resInnerContent}>
+                            <li>test</li>
+                            <li>test</li>
+                            <li>test</li>
+                            <li>test</li>
+                            <li>test</li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <div>
+                            job search{" "}
+                            <ArrowForwardIosIcon
+                                className={classes.resHeaderArrow}
+                            />
+                        </div>
+
+                        <ul className={classes.resInnerContent}>
+                            <li>test</li>
+                            <li>test</li>
+                            <li>test</li>
+                            <li>test</li>
+                            <li>test</li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <div>
+                            lodgment{" "}
+                            <ArrowForwardIosIcon
+                                className={classes.resHeaderArrow}
+                            />
+                        </div>
+
+                        <ul className={classes.resInnerContent}>
+                            <li>test</li>
+                            <li>test</li>
+                            <li>test</li>
+                            <li>test</li>
+                            <li>test</li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
--- src/layout/header/styles.js
+++ src/layout/header/styles.js
@@ -1,28 +1,160 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-    header: {
-        background: '#f4f4f4'
+    topHeader: {
+        background: "#f4f4f4",
     },
-    headerWrap: {
-        display: 'flex',
-        justifyContent: 'flex-end',
-        margin: '0 auto',
-        width: '90%',
+    topHeaderWrap: {
+        display: "flex",
+        justifyContent: "flex-end",
+        margin: "0 auto",
+        width: "90%",
         "& > div": {
-            padding: '0 10px',
+            padding: "0 10px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+        },
+    },
+
+    headerLogoWrap: {
+        display: "flex",
+        justifyContent: "center",
+    },
+
+    headerLogo: {
+        margin: "20px 0",
+    },
+
+    bottomHeader: {
+        width: "90%",
+        margin: "0 auto",
+    },
+
+    bottomHeaderContent: {
+        display: "flex",
+        justifyContent: "space-around",
+        background: "#b72a0f",
+        maxHeight: "58px",
+        "& > li": {
+            listStyle: "none",
+            "&:hover": {
+                "& $ul": {
+                    display: "block",
+                },
+            },
+        },
+        "& > li > p": {
+            padding: "20px 50px",
+            color: "#f2f2f2",
+            "&:hover": {
+                background: "#911e09",
+            },
+        },
+    },
+
+    innerHeaderContent: {
+        background: "#b72a0f",
+        display: "none",
+        "& > li": {
+            listStyle: "none",
+            padding: "15px 0",
+            textAlign: "center",
+            fontSize: "90%",
+            color: "#f2f2f2",
+            "&:hover": {
+                cursor: "pointer",
+                background: "#911e09",
+            },
+        },
+    },
+
+    resHeader: {
+        display: "none",
+    },
+    
+    overlay: {
+        width: '100%',
+        height: '100%',
+        background: '#111',
+        position: 'fixed',
+        top: '0',
+        display: 'none'
+    },
+
+    resClose: {
+        color: '#fff',
+        fontSize: '35px',
+        position: 'absolute',
+        right: '20px'
+    },
+
+    resHeaderWrap: {
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '20px',
+        borderBottom: '1px solid grey',
+        marginBottom: '15px'
+    },
+
+    resHeaderContent: {
+        width: '60%',
+        margin: '0 auto',
+        "& > li": {
+            listStyle: 'none',
+            "&:hover": {
+                "& $ul": {
+                    display: 'block'
+                }
+            }
+        },
+        "& > li > div": {
             display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
+            justifyContent: 'space-between',
+            padding: '5px 30px',
+            color: '#fff',
+            borderBottom: '1px solid grey',
+            padding: '20px',
         }
     },
 
-    // Responsive Design
-    [theme.breakpoints.down('md')]: {
-        // do something
+    resInnerContent: {
+        display: 'none',
+        "& > li": {
+            color: '#f2f2f2',
+            marginTop: '10px',
+            marginLeft: '40px',
+            marginBottom: '15px'
+        }
     },
-    [theme.breakpoints.down('xs')]: {
-       // do something
+
+    resHeaderArrow: {
+        fontSize: '16px'
+    },
+
+    // Responsive Design
+    [theme.breakpoints.down("md")]: {
+        bottomHeaderContent: {
+            "& > li > p": {
+                padding: "20px",
+            },
+        },
+    },
+    [theme.breakpoints.down("xs")]: {
+        desktopViewHeader: {
+            display: "none",
+        },
+        resHeader: {
+            background: "#b72a0f",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: 'center',
+            padding: "10px 20px",
+        },
+        resHeaderLogo: {
+            width: "100px",
+            filter: "invert(100%)"
+        },
     },
 }));
 
